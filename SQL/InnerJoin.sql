428d094c862556f63b783eddff2031dd90ff565b
wupangyen <72144016+wupangyen@users.noreply.github.com>
2022-01-24 15:43:10 -0500

    Update InnerJoin.sql

--- SQL/InnerJoin.sql
+++ SQL/InnerJoin.sql
@@ -1,11 +1,15 @@
-inner join, selects records that have matching values in both tables
+/*inner join, selects records that have matching values in both tables
 
-example:
+example:*/
 select Orders.OrderID, Customers.CustomerName, Orders.OrderDate
 From Orders
 Inner join Customers ON Orders.CustomerID=CUstomers.CustomerID
 
-
+/*
 Inner Join: Returns records that have matching values in both tables
 
-Left Join: Returns all records from the lef
+Left Join: Returns all records from the left table, and the matched records from the right table
+right join: returns all records from the right table, and the matched records from the left table
+full join : returns all records when there is a match in either left or right join
+
+*/
