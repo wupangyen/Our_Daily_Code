86ee956e7897a569ddb4b68810ae5f059083cec2
wupangyen <72144016+wupangyen@users.noreply.github.com>
2022-01-24 15:40:57 -0500

    Update and rename inner join to InnerJoin.sql

--- SQL/InnerJoin.sql
+++ SQL/InnerJoin.sql
@@ -4,3 +4,8 @@ example:
 select Orders.OrderID, Customers.CustomerName, Orders.OrderDate
 From Orders
 Inner join Customers ON Orders.CustomerID=CUstomers.CustomerID
+
+
+Inner Join: Returns records that have matching values in both tables
+
+Left Join: Returns all records from the lef
