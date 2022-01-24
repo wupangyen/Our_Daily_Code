b02724c9b262c690dfe1836e64f2ab8806a37a89
wupangyen <72144016+wupangyen@users.noreply.github.com>
2022-01-24 16:22:42 -0500

    Update InnerJoin.sql

--- SQL/InnerJoin.sql
+++ SQL/InnerJoin.sql
@@ -7,8 +7,12 @@ Inner join Customers ON Orders.CustomerID=CUstomers.CustomerID
 
 /*
 Inner Join: Returns records that have matching values in both tables
+If there are records in the "Order" table that do not have matches in "Customers", these orders will not be shown!
+
 
 Left Join: Returns all records from the left table, and the matched records from the right table
+returns all records from the left table, and the matching records from the right table. The result is 0 records from the right side, if there is no match 
+
 right join: returns all records from the right table, and the matched records from the left table
 full join : returns all records when there is a match in either left or right join
 
